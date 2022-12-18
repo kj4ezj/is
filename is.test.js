b7a15307421e77a2d6cc970353b69a206b10a00d
Zach Butler <kj4ezj@pm.me>
2022-12-17 22:32:12 -0500

    Comments

--- is.test.js
+++ is.test.js
@@ -1,8 +1,10 @@
 /* eslint {no-new-wrappers:0} */
 const is = require('./is.js');
 
+/* test fixtures */
 const WHITESPACE = '  \v  \t \r\n    \n\f ';
 
+/* test cases */
 afterEach(() => {
     jest.clearAllMocks();
 });
@@ -10,10 +12,11 @@ afterEach(() => {
 describe('is.js', () => {
     test('exists', () => expect(is).toBeTruthy());
 
+    /* is.nullOrEmpty() unit tests */
     describe('is.nullOrEmpty()', () => {
         test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
 
-        describe('given a function that returns', () => {
+        describe('given a function that returns', () => { // these should all be false, functions are non-empty for our purposes
             test('false', () => expect(is.nullOrEmpty(() => false)).toEqual(false));
             test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(() => true)).toEqual(false));
@@ -29,17 +32,18 @@ describe('is.js', () => {
             test('whitespace', () => expect(is.nullOrEmpty(WHITESPACE)).toEqual(true));
         });
 
-        describe('given a primitive object', () => {
+        describe('given a primitive object', () => { // primitive objects are treated differently than primitive literals sometimes
             test('false', () => expect(is.nullOrEmpty(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(new Boolean(true))).toEqual(false));
             test('whitespace', () => expect(is.nullOrEmpty(new String(WHITESPACE))).toEqual(true));
         });
     });
 
+    /* is.string() unit tests */
     describe('is.string()', () => {
         test('exists', () => expect(is.string).toBeTruthy());
 
-        describe('given a function that returns', () => {
+        describe('given a function that returns', () => { // these should all be false, functions aren't strings
             test('false', () => expect(is.string(() => false)).toEqual(false));
             test('null', () => expect(is.string(() => null)).toEqual(false));
             test('true', () => expect(is.string(() => true)).toEqual(false));
@@ -55,7 +59,7 @@ describe('is.js', () => {
             test('whitespace', () => expect(is.string(WHITESPACE)).toEqual(true));
         });
 
-        describe('given a primitive object', () => {
+        describe('given a primitive object', () => { // primitive objects are treated differently than primitive literals sometimes
             test('false', () => expect(is.string(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.string(new Boolean(true))).toEqual(false));
             test('whitespace', () => expect(is.string(new String(WHITESPACE))).toEqual(true));
