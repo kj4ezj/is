5c4d37ee21d85dd3012e45be5fe452b16c711ebd
Zach Butler <kj4ezj@pm.me>
2022-12-17 20:28:45 -0500

    Address security vulnerability described at https://eslint.org/docs/latest/rules/no-prototype-builtins

--- is.js
+++ is.js
@@ -1,3 +1,3 @@
 /* eslint {no-restricted-syntax:0, no-undef:0} */
 module.exports.nullOrEmpty = thing => thing === null || thing === undefined || (Array.isArray(thing) && thing.length === 0) || (thing.constructor === Object && objectIsEmpty(thing)) || /^\s*$/.test(thing);
-objectIsEmpty = obj => { for (const prop in obj) if (obj.hasOwnProperty(prop)) return false; return true; };
+objectIsEmpty = obj => { for (const prop in obj) if (Object.prototype.hasOwnProperty.call(obj, prop)) return false; return true; };
