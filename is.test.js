7fd55144c49ee48d9dd128e6caace871818f8e54
Zach Butler <kj4ezj@pm.me>
2022-12-17 22:20:39 -0500

    Tests with whitespace

--- is.test.js
+++ is.test.js
@@ -1,6 +1,8 @@
 /* eslint {no-new-wrappers:0} */
 const is = require('./is.js');
 
+const WHITESPACE = '  \v  \t \r\n    \n\f ';
+
 afterEach(() => {
     jest.clearAllMocks();
 });
@@ -16,6 +18,7 @@ describe('is.js', () => {
             test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(() => true)).toEqual(false));
             test('undefined', () => expect(is.nullOrEmpty(() => undefined)).toEqual(false));
+            test('whitespace', () => expect(is.nullOrEmpty(() => WHITESPACE)).toEqual(false));
         });
 
         describe('given a literal', () => {
@@ -23,11 +26,13 @@ describe('is.js', () => {
             test('null', () => expect(is.nullOrEmpty(null)).toEqual(true));
             test('true', () => expect(is.nullOrEmpty(true)).toEqual(false));
             test('undefined', () => expect(is.nullOrEmpty(undefined)).toEqual(true));
+            test('whitespace', () => expect(is.nullOrEmpty(WHITESPACE)).toEqual(true));
         });
 
         describe('given a primitive object', () => {
             test('false', () => expect(is.nullOrEmpty(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(new Boolean(true))).toEqual(false));
+            test('whitespace', () => expect(is.nullOrEmpty(new String(WHITESPACE))).toEqual(true));
         });
     });
 
@@ -39,6 +44,7 @@ describe('is.js', () => {
             test('null', () => expect(is.string(() => null)).toEqual(false));
             test('true', () => expect(is.string(() => true)).toEqual(false));
             test('undefined', () => expect(is.string(() => undefined)).toEqual(false));
+            test('whitespace', () => expect(is.string(() => WHITESPACE)).toEqual(false));
         });
 
         describe('given a literal', () => {
@@ -46,11 +52,13 @@ describe('is.js', () => {
             test('null', () => expect(is.string(null)).toEqual(false));
             test('true', () => expect(is.string(true)).toEqual(false));
             test('undefined', () => expect(is.string(undefined)).toEqual(false));
+            test('whitespace', () => expect(is.string(WHITESPACE)).toEqual(true));
         });
 
         describe('given a primitive object', () => {
             test('false', () => expect(is.string(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.string(new Boolean(true))).toEqual(false));
+            test('whitespace', () => expect(is.string(new String(WHITESPACE))).toEqual(true));
         });
     });
 });
