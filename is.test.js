765c4a3aa4c3bda3bfc39fdb3cf901e91acc72b0
Zach Butler <kj4ezj@pm.me>
2022-12-17 20:54:31 -0500

    Test skeleton for is.nullOrEmpty()

--- is.test.js
+++ is.test.js
@@ -6,4 +6,8 @@ afterEach(() => {
 
 describe('is.js', () => {
     test('exists', () => expect(is).toBeTruthy());
+
+    describe('is.nullOrEmpty()', () => {
+        test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
+    });
 });
