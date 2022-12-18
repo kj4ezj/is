1062cd7f35da7a9d62ee63f7871e34cd049f3459
Zach Butler <kj4ezj@pm.me>
2022-12-17 23:16:01 -0500

    Use uppercase FALSE const for function tests to help avoid copy/pasta mistakes and confusion in diffs

--- is.test.js
+++ is.test.js
@@ -2,6 +2,7 @@
 const is = require('./is.js');
 
 /* test fixtures */
+const FALSE = false;
 const POSITIVE_NUMBER = 18650;
 const STRING = 'The quick brown fox jumps over the lazy dog';
 const WHITESPACE = '  \v  \t \r\n    \n\f ';
@@ -19,18 +20,18 @@ describe('is.js', () => {
         test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
 
         describe('given a function that returns', () => { // these should all be false, functions are non-empty for our purposes
-            test('empty string literal', () => expect(is.nullOrEmpty(() => '')).toEqual(false));
-            test('empty string object', () => expect(is.nullOrEmpty(() => new String(''))).toEqual(false));
-            test('false', () => expect(is.nullOrEmpty(() => false)).toEqual(false));
-            test('non-empty string', () => expect(is.nullOrEmpty(() => STRING)).toEqual(false));
-            test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(false));
-            test('number literal', () => expect(is.nullOrEmpty(() => POSITIVE_NUMBER)).toEqual(false));
-            test('number object', () => expect(is.nullOrEmpty(() => new Number(POSITIVE_NUMBER))).toEqual(false));
-            test('true', () => expect(is.nullOrEmpty(() => true)).toEqual(false));
-            test('undefined', () => expect(is.nullOrEmpty(() => undefined)).toEqual(false));
-            test('whitespace', () => expect(is.nullOrEmpty(() => WHITESPACE)).toEqual(false));
-            test('zero literal', () => expect(is.nullOrEmpty(() => 0)).toEqual(false));
-            test('zero object', () => expect(is.nullOrEmpty(() => new Number(0))).toEqual(false));
+            test('empty string literal', () => expect(is.nullOrEmpty(() => '')).toEqual(FALSE));
+            test('empty string object', () => expect(is.nullOrEmpty(() => new String(''))).toEqual(FALSE));
+            test('false', () => expect(is.nullOrEmpty(() => false)).toEqual(FALSE));
+            test('non-empty string', () => expect(is.nullOrEmpty(() => STRING)).toEqual(FALSE));
+            test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(FALSE));
+            test('number literal', () => expect(is.nullOrEmpty(() => POSITIVE_NUMBER)).toEqual(FALSE));
+            test('number object', () => expect(is.nullOrEmpty(() => new Number(POSITIVE_NUMBER))).toEqual(FALSE));
+            test('true', () => expect(is.nullOrEmpty(() => true)).toEqual(FALSE));
+            test('undefined', () => expect(is.nullOrEmpty(() => undefined)).toEqual(FALSE));
+            test('whitespace', () => expect(is.nullOrEmpty(() => WHITESPACE)).toEqual(FALSE));
+            test('zero literal', () => expect(is.nullOrEmpty(() => 0)).toEqual(FALSE));
+            test('zero object', () => expect(is.nullOrEmpty(() => new Number(0))).toEqual(FALSE));
         });
 
         describe('given a literal', () => {
@@ -61,18 +62,18 @@ describe('is.js', () => {
         test('exists', () => expect(is.string).toBeTruthy());
 
         describe('given a function that returns', () => { // these should all be false, functions aren't strings
-            test('empty string literal', () => expect(is.string(() => '')).toEqual(false));
-            test('empty string object', () => expect(is.string(() => new String(''))).toEqual(false));
-            test('false', () => expect(is.string(() => false)).toEqual(false));
-            test('non-empty string', () => expect(is.string(() => STRING)).toEqual(false));
-            test('null', () => expect(is.string(() => null)).toEqual(false));
-            test('number literal', () => expect(is.string(() => POSITIVE_NUMBER)).toEqual(false));
-            test('number object', () => expect(is.string(() => new Number(POSITIVE_NUMBER))).toEqual(false));
-            test('true', () => expect(is.string(() => true)).toEqual(false));
-            test('undefined', () => expect(is.string(() => undefined)).toEqual(false));
-            test('whitespace', () => expect(is.string(() => WHITESPACE)).toEqual(false));
-            test('zero literal', () => expect(is.string(() => 0)).toEqual(false));
-            test('zero object', () => expect(is.string(() => new Number(0))).toEqual(false));
+            test('empty string literal', () => expect(is.string(() => '')).toEqual(FALSE));
+            test('empty string object', () => expect(is.string(() => new String(''))).toEqual(FALSE));
+            test('false', () => expect(is.string(() => false)).toEqual(FALSE));
+            test('non-empty string', () => expect(is.string(() => STRING)).toEqual(FALSE));
+            test('null', () => expect(is.string(() => null)).toEqual(FALSE));
+            test('number literal', () => expect(is.string(() => POSITIVE_NUMBER)).toEqual(FALSE));
+            test('number object', () => expect(is.string(() => new Number(POSITIVE_NUMBER))).toEqual(FALSE));
+            test('true', () => expect(is.string(() => true)).toEqual(FALSE));
+            test('undefined', () => expect(is.string(() => undefined)).toEqual(FALSE));
+            test('whitespace', () => expect(is.string(() => WHITESPACE)).toEqual(FALSE));
+            test('zero literal', () => expect(is.string(() => 0)).toEqual(FALSE));
+            test('zero object', () => expect(is.string(() => new Number(0))).toEqual(FALSE));
         });
 
         describe('given a literal', () => {
