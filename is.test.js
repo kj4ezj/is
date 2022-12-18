c6c8c542a484053e60c0c4fc5f8d1108f43898bb
Zach Butler <kj4ezj@pm.me>
2022-12-17 22:36:04 -0500

    Tests for empty strings

--- is.test.js
+++ is.test.js
@@ -17,6 +17,8 @@ describe('is.js', () => {
         test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
 
         describe('given a function that returns', () => { // these should all be false, functions are non-empty for our purposes
+            test('empty string literal', () => expect(is.nullOrEmpty(() => '')).toEqual(false));
+            test('empty string object', () => expect(is.nullOrEmpty(() => new String(''))).toEqual(false));
             test('false', () => expect(is.nullOrEmpty(() => false)).toEqual(false));
             test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(() => true)).toEqual(false));
@@ -25,6 +27,7 @@ describe('is.js', () => {
         });
 
         describe('given a literal', () => {
+            test('empty string', () => expect(is.nullOrEmpty('')).toEqual(true));
             test('false', () => expect(is.nullOrEmpty(false)).toEqual(false));
             test('null', () => expect(is.nullOrEmpty(null)).toEqual(true));
             test('true', () => expect(is.nullOrEmpty(true)).toEqual(false));
@@ -33,6 +36,7 @@ describe('is.js', () => {
         });
 
         describe('given a primitive object', () => { // primitive objects are treated differently than primitive literals sometimes
+            test('empty string', () => expect(is.nullOrEmpty(new String(''))).toEqual(true));
             test('false', () => expect(is.nullOrEmpty(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.nullOrEmpty(new Boolean(true))).toEqual(false));
             test('whitespace', () => expect(is.nullOrEmpty(new String(WHITESPACE))).toEqual(true));
@@ -44,6 +48,8 @@ describe('is.js', () => {
         test('exists', () => expect(is.string).toBeTruthy());
 
         describe('given a function that returns', () => { // these should all be false, functions aren't strings
+            test('empty string literal', () => expect(is.string(() => '')).toEqual(false));
+            test('empty string object', () => expect(is.string(() => new String(''))).toEqual(false));
             test('false', () => expect(is.string(() => false)).toEqual(false));
             test('null', () => expect(is.string(() => null)).toEqual(false));
             test('true', () => expect(is.string(() => true)).toEqual(false));
@@ -52,6 +58,7 @@ describe('is.js', () => {
         });
 
         describe('given a literal', () => {
+            test('empty string', () => expect(is.string('')).toEqual(true));
             test('false', () => expect(is.string(false)).toEqual(false));
             test('null', () => expect(is.string(null)).toEqual(false));
             test('true', () => expect(is.string(true)).toEqual(false));
@@ -60,6 +67,7 @@ describe('is.js', () => {
         });
 
         describe('given a primitive object', () => { // primitive objects are treated differently than primitive literals sometimes
+            test('empty string', () => expect(is.string(new String(''))).toEqual(true));
             test('false', () => expect(is.string(new Boolean(false))).toEqual(false));
             test('true', () => expect(is.string(new Boolean(true))).toEqual(false));
             test('whitespace', () => expect(is.string(new String(WHITESPACE))).toEqual(true));
