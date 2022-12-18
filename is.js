50cc5db801be39e244d6cf71873cae1c72bbbf46
Zach Butler <kj4ezj@pm.me>
2022-12-17 20:23:12 -0500

    Disable no-undef eslint rule only in is.js

--- is.js
+++ is.js
@@ -1,2 +1,3 @@
+/* eslint {no-undef:0} */
 module.exports.nullOrEmpty = thing => thing === null || thing === undefined || (Array.isArray(thing) && thing.length === 0) || (thing.constructor === Object && objectIsEmpty(thing)) || /^\s*$/.test(thing);
 objectIsEmpty = obj => { for (const prop in obj) if (obj.hasOwnProperty(prop)) return false; return true; };
