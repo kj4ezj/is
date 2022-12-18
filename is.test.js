64d82143806fa2e75caed11ace6e1e91aa853640
Zach Butler <kj4ezj@pm.me>
2022-12-17 21:16:35 -0500

    Tests for false

--- is.test.js
+++ is.test.js
@@ -11,11 +11,13 @@ describe('is.js', () => {
         test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
 
         describe('given', () => {
+            test('false', () => expect(is.nullOrEmpty(false)).toEqual(false));
             test('null', () => expect(is.nullOrEmpty(null)).toEqual(true));
             test('undefined', () => expect(is.nullOrEmpty(undefined)).toEqual(true));
         });
 
         describe('given a function that returns', () => {
+            test('false', () => expect(is.nullOrEmpty(() => false)).toEqual(false));
             test('null', () => expect(is.nullOrEmpty(() => null)).toEqual(false));
             test('undefined', () => expect(is.nullOrEmpty(() => undefined)).toEqual(false));
         });
@@ -25,11 +27,13 @@ describe('is.js', () => {
         test('exists', () => expect(is.string).toBeTruthy());
 
         describe('given', () => {
+            test('false', () => expect(is.string(false)).toEqual(false));
             test('null', () => expect(is.string(null)).toEqual(false));
             test('undefined', () => expect(is.string(undefined)).toEqual(false));
         });
 
         describe('given a function that returns', () => {
+            test('false', () => expect(is.string(() => false)).toEqual(false));
             test('null', () => expect(is.string(() => null)).toEqual(false));
             test('undefined', () => expect(is.string(() => undefined)).toEqual(false));
         });
