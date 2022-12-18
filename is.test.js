e34e23e53876ad5cc92c7459bbb8cba8b737705e
Zach Butler <kj4ezj@pm.me>
2022-12-17 21:03:10 -0500

    Create some test groups to organize our test cases

--- is.test.js
+++ is.test.js
@@ -9,9 +9,21 @@ describe('is.js', () => {
 
     describe('is.nullOrEmpty()', () => {
         test('exists', () => expect(is.nullOrEmpty).toBeTruthy());
+
+        describe('given', () => {
+        });
+
+        describe('given a function that returns', () => {
+        });
     });
 
     describe('is.string()', () => {
         test('exists', () => expect(is.string).toBeTruthy());
+
+        describe('given', () => {
+        });
+
+        describe('given a function that returns', () => {
+        });
     });
 });
