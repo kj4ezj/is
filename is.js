3eda2fe2da4216d48065d206315c433ff203fa08
Zach Butler <kj4ezj@pm.me>
2022-12-18 21:30:51 -0500

    Remove hasOwnProperty() check for now

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-/* eslint {no-undef:0} */
+/* eslint {guard-for-in: 0, no-undef:0} */
 module.exports.nullOrEmpty = thing => thing === null || thing === undefined || (Array.isArray(thing) && thing.length === 0) || (thing.constructor === Object && objectIsEmpty(thing)) || /^\s*$/.test(thing);
-objectIsEmpty = obj => { for (const prop in obj) if (Object.prototype.hasOwnProperty.call(obj, prop)) return false; return true; };
+objectIsEmpty = obj => { for (const prop in obj) return false; return true; }; // eslint-disable-line no-unreachable-loop
 module.exports.string = obj => Object.prototype.toString.call(obj) === '[object String]';
